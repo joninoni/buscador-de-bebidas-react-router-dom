c72376ee821e0f3cb6c6c043c4969f2576b6d32b
joninoni <jonibeltranhayer@gmail.com>
2024-10-27 23:27:00 -0600

    muestro la informacion de la receta en el modal y agrego la funcionalidad de cerrar el modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,11 +1,30 @@
 import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react';
 import { Fragment } from 'react';
 import useAppStore from '../stores/useAppStore';
+import { CurrencyDrink } from '../types';
  
 export default function Modal() {
 
     const modal = useAppStore( state => state.modal)
     const closeModal = useAppStore( state => state.closeModal)
+    const currencyDrink = useAppStore( state => state.currencyDrink)
+
+    const renderIngredients = () =>{
+        const ingredients : JSX.Element[] = []
+        for(let i = 1; i <= 6;i++){
+            const ingredient = currencyDrink[`strIngredient${i}` as keyof CurrencyDrink] 
+            const measure = currencyDrink[`strMeasure${i}` as keyof CurrencyDrink]
+
+            if(ingredient && measure){
+                ingredients.push(
+                    <li>
+                        {ingredient} - {measure}
+                    </li>
+                )
+            }
+        }
+        return ingredients
+    }
     
     return (
         <>
@@ -36,15 +55,35 @@ export default function Modal() {
                         >
                             <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl sm:p-6" >
                                 <DialogTitle as="h3" className="text-gray-900 text-4xl      font-extrabold my-5 text-center">
-                                    Titulo Aquí
+                                    {currencyDrink.strDrink}
                                 </DialogTitle>
+
+                                <img
+                                    src={currencyDrink.strDrinkThumb}
+                                    alt={`Imagen de ${currencyDrink.strDrink}`}
+                                    className='mx-auto w-96'
+                                />
                                 <DialogTitle as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
-                                Ingredientes y Cantidades
+                                    <p className='py-2'>Ingredientes y cantidades</p>
                                 </DialogTitle>
+                                    {renderIngredients()}
                                 <DialogTitle as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
-                                Instrucciones
+                                    <p className='py-2'>Instrucciones</p>
                                 </DialogTitle>
-                            </DialogPanel>
+                                    <p>{currencyDrink.strInstructions}</p>
+
+                                    <div className='flex  gap-4'>
+                                        <button className='bg-orange-400 py-5 w-full mt-5 rounded-lg hover:bg-orange-500'>
+                                            Añadir a favoritos
+                                        </button>
+                                        <button
+                                            className='bg-gray-400 py-5 w-full mt-5 rounded-lg'
+                                            onClick={() => closeModal()}
+                                        >
+                                            Cerrar
+                                        </button>  
+                                    </div>                                      
+                            </DialogPanel>                            
                         </TransitionChild>
                     </div>
                 </div>
