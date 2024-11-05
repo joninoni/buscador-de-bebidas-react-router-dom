59484ae6802b13b9ac5c690c035bfb02484d7c2a
joninoni <jonibeltranhayer@gmail.com>
2024-11-05 17:21:29 -0600

    muestro los favoriritos guardados por el estado favorites

--- src/views/FavoritesPage.tsx
+++ src/views/FavoritesPage.tsx
@@ -1,6 +1,31 @@
+import { useMemo } from "react"
+import { DrinkCard } from "../components/DrinkCard"
+import useAppStore from "../stores/useAppStore"
+
 const FavoritesPage = () => {
+
+    const favorites = useAppStore(state => state.favorites)
+    const hasFavorites = useMemo( ()=> favorites.length ,[favorites])
+
     return (
-        <h1>Favoritos</h1>
+
+        <section>
+            <h1 className="text-6xl font-extrabold">Favoritos</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-12">
+                {hasFavorites ?
+                    (
+                        favorites.map(drink => (
+                            <DrinkCard
+                                key={drink.idDrink}
+                                drink={drink}
+                            />
+                        ))
+                    ) : (
+                        <p className="my-5 text-center">Aqui apareceran los favoritos</p>
+                    )
+                }
+            </div>
+        </section>
     )
 }
 
